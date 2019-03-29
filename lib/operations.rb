e0c584525eb48536cb9dcfd0839451e9f974e7b0
Henry Koehler <henryk1229@gmail.com>
2019-03-29 19:28:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,15 @@
 def unsafe?(speed)
-
+  if speed < 40
+    true
+  elsif speed > 60
+    true
+  else 
+    false 
+  end 
 end
 
-
-
 def not_safe?(speed)
-	
+	speed != arr ? true : false 
 end
 	
 
